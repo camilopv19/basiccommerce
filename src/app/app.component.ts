e7a990fc23a7f8cec2b70ca5d0b3411a6d1f8021
Camilo P <kmilopv@gmail.com>
2018-12-03 20:27:23 -0500

    Directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,47 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   // title = 'basiccommerce';
-  courses = [];
+  courses;
   post = {
     title: "Title",
     isFavorite: false
+  };
+
+  viewMode = '';
+  active = false;
+  canSave = true;
+  canSave1 = false;
+  task = {
+    title: 'Review Apps',
+    assignee:{
+      name: 'Nataly Rojas'
+    }
+  };
+
+  onAdd(){
+    this.courses.push({ id: 4, name: 'Popcorn' })
   }
 
-  viewMode = 'map';
+  onRemove(course){
+    let index = this.courses.indexOf(course);
+    this.courses.splice(index, 1)
+  }
+
+  onChange(course){
+    course.name = 'Updated';
+  }
+
+  loadCourses(){
+    this.courses = [
+      { id: 1, name: 'Burguers '},
+      { id: 2, name: 'Hotdogs '},
+      { id: 2, name: 'Pretzels '}
+    ];
+  }
+
+  trackCourse(index, course){
+    return course ? course.id : undefined;
+  }
 
   onFavoriteChange(eventArgs: FavoriteChangedEventArgs){
     console.log("Favorite changed: ", eventArgs);
